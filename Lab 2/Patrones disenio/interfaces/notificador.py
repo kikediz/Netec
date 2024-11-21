491eb585a0bfa854c687d528390cce2a013c95d8
Alfonso Enrique Diaz Malaver <alendiaz@bancolombia.com.co>
2024-11-21 07:43:52 -0500

    Adding design patterns

---
+++ Lab 2/Patrones disenio/interfaces/notificador.py
@@ -0,0 +1,7 @@
+from abc import ABC, abstractmethod 
+
+class Notificador(ABC): 
+    
+    @abstractmethod 
+    def enviar(self, mensaje: str, destinatario: str) -> None: 
+        pass
---
+++ Lab 2/Patrones disenio/servicios/notificador_sms.py
@@ -0,0 +1,5 @@
+from interfaces.notificador import Notificador 
+
+class NotificadorSMS(Notificador): 
+    def enviar(self, mensaje: str, destinatario: str) -> None: 
+        print(f"Enviando SMS a {destinatario}: {mensaje}")
